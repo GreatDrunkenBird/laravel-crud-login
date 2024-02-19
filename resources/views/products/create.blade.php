0bd4debb3d6b9245f70e91a9763f210ee3d5d5e9
GreatDrunkenBird <filiperelly@outlook.com>
2024-02-19 16:00:03 -0300

    Not done

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -1,5 +1,9 @@
 <x-app-layout>
-    <h1>Create a Product</h1>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Create a Product') }}
+        </h2>
+    </x-slot>
     <div>
         @if($errors->any())
             <ul>
@@ -11,24 +15,25 @@
             </ul>
         @endif
     </div>
-    <form method="post" action="{{route('product.store')}}">
+    <form class="max-w-7xl mx-auto sm:px-6 lg:px-8 py-8 text-center" method="post" action="{{route('product.store')}}">
         @csrf
         @method('post')
-        <div>
+        <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg p-4 text-gray-900 dark:text-gray-100">
             <label>Name</label>
             <input type="text" name="name" placeholder="Name" />
 
             <label>Quantity</label>
-            <input type="text" name="qty" placeholder="Quantity" />
+            <input type="text" name="quantity" placeholder="Quantity" />
 
             <label>Price</label>
             <input type="text" name="price" placeholder="Price" />
 
             <label>Description</label>
             <input type="text" name="description" placeholder="Description" />
-        </div>
-        <div>
-            <input type="submit" value="Save a New Product" />
+
+            <button class="pt-4 font-bold hover:text-green-500">
+                <input type="submit" value="Save a New Product" />
+            </button>
         </div>
     </form>
 </x-app-layout>
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,42 +1,47 @@
 <x-app-layout>
     @if(session()->has('success'))
-
-        <div class="bg-green-300 dark:bg-green-600 shadow-sm sm:rounded-lg font-bold text-center text-gray-900 dark:text-gray-100 max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <h6 class="bg-green-300 dark:bg-green-600 shadow-sm sm:rounded-lg font-bold text-center text-gray-900 dark:text-gray-100 max-w-7xl mx-auto sm:px-6 lg:px-8">
             {{session('success')}}
-        </div>
-
-    @else
-        <div class="bg-white dark:bg-grey-800 shadow-sm sm:rounded-lg text-center text-gray-900 dark:text-gray-100 max-w-7xl mx-auto sm:px-6 lg:px-8">
-            Manage products
-        </div>
+        </h6>
     @endif
     <div class="py-12">
-        <div class="text-right text-gray-900 dark:text-gray-100 max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div>
-                <a href="{{ route('product.create') }}">
-                    Create a Product
-                </a>
-            </div>
-            <div class="bg-white dark:bg-gray-800 shadow-sm sm:rounded-lg">
-                <table class="border-separate border-spacing-x-12">
-                    <tr>
-                        <th>ID</th>
-                        <th>Name</th>
-                        <th>Quantity</th>
-                        <th>Price</th>
-                        <th>Description</th>
-                    </tr>
+        <div class="text-right text-gray-900 dark:text-gray-100 max-w-4xl mx-auto sm:px-6 lg:px-8">
+
+            <div class="align-middle bg-white dark:bg-gray-800 shadow-sm sm:rounded-lg">
+
+                <table class="table-fixed border-separate border-spacing-x-8">
+
+                    <thead>
+                        <tr>
+
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Quantity</th>
+                            <th>Price</th>
+                            <th>Description</th>
+                            <th class="sm:flex sm:items-center sm:ms-48 hover:text-green-500">
+                                <a href="{{ route('product.create') }}">
+                                    Adicionar Produto
+                                </a>
+                            </th>
+
+                        </tr>
+                    </thead>
+
+                    <tbody>
                     @foreach($products as $product)
                         <tr>
+
                             <th>{{$product->id}}</th>
                             <td>{{$product->name}}</td>
                             <td>{{$product->qty}}</td>
                             <td>{{$product->price}}</td>
                             <td>{{$product->description}}</td>
-                            <td class="text-right sm:flex sm:items-center sm:ms-6">
+
+                            <td>
                                 <x-dropdown width="48">
                                     <x-slot name="trigger">
-                                        <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
+                                        <button class="inline-flex items-center px-5 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
                                             <div>Options</div>
 
                                             <div class="ms-1">
@@ -65,10 +70,14 @@
 
                                     </x-slot>
                                 </x-dropdown>
+
                             </td>
+
                         </tr>
                     @endforeach
+                    </tbody>
                 </table>
+
             </div>
         </div>
     </div>
